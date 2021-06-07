4254c15b4ebcb91fed0b24685ee04774e4f6db1a
SASUPERNOVA <agentrip@gmail.com>
2021-06-07 14:58:24 +0300

    Add toggleswitch-toggle event

--- src/components/Alarm/Alarm.js
+++ src/components/Alarm/Alarm.js
@@ -5,6 +5,17 @@
         const root = this.shadowRoot;
 
         root.querySelector('#delete-button').addEventListener('click', deleteClick);
+
+        this.addEventListener('toggleswitch-toggle', alarmSet);
+    }
+
+    function alarmSet(ev) {
+        if (ev.detail.checked) {
+            new Notification('Alarm', {body: 'An alarm has been set...'});
+        }
+        else {
+            new Notification('Alarm', {body: 'An alarm has been disabled...'});
+        }
     }
 
     function deleteClick(ev) {
--- src/components/ToggleSwitch/ToggleSwitch.js
+++ src/components/ToggleSwitch/ToggleSwitch.js
@@ -1,3 +1,14 @@
 (() => {
-    loadComponent('components/ToggleSwitch/ToggleSwitch.html', 'toggle-switch');
+    loadComponent('components/ToggleSwitch/ToggleSwitch.html', 'toggle-switch', eventListeners);
+
+    function eventListeners() {
+        const root = this.shadowRoot;
+
+        root.querySelector('#checkbox').addEventListener('change', checkboxChange);
+    }
+    
+    function checkboxChange(ev) {
+        const checked = ev.target.checked;
+        parentComponent(parentComponent(ev.target)).dispatchEvent(new CustomEvent('toggleswitch-toggle', {detail: { checked: checked }}));
+    }
 })();
