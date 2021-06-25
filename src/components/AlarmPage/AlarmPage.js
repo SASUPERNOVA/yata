1eb81305e1d7892ab4bb837ad8b35ca3ae324573
SASUPERNOVA <agentrip@gmail.com>
2021-06-25 20:55:59 +0300

    change AlarmPage.alarms structure and add event handling for Alarm input

--- src/components/AlarmPage/AlarmPage.js
+++ src/components/AlarmPage/AlarmPage.js
@@ -1,6 +1,6 @@
 (() => {
     class AlarmPage extends WebComponent {
-        static alarms = [];
+        static alarms = {};
         static initialized = false;
         constructor() {
             super('components/AlarmPage/AlarmPage.html', 'alarm-page');
@@ -11,8 +11,8 @@
             this.addEventListener('addbutton-click', (ev) => this.onAddButtonClick(ev));
             this.addEventListener('timer-finished', ev => this.onTimerFinished(ev));
             this.addEventListener('data-received', ev => this.onDataReceived(ev));
-            for (const alarm of AlarmPage.alarms) {
-                this.shadowRoot.querySelector('main').appendChild(alarm);
+            for (const alarm of Object.values(AlarmPage.alarms)) {
+                this.shadowRoot.querySelector('main').appendChild(alarm.component);
             }
             if (!AlarmPage.initialized) {
                 yanuAPI.loadFile('AlarmPage.json');
@@ -22,8 +22,15 @@
 
         onAddButtonClick(ev) {
             const alarmComponent = document.createElement('alarm-component');
-            AlarmPage.alarms.push(alarmComponent); 
+            //AlarmPage.alarms.push(alarmComponent);
             this.shadowRoot.querySelector('main').appendChild(alarmComponent);
+            alarmComponent.onShadowRootReady(() => {
+                AlarmPage.alarms[alarmComponent.refId] = {
+                    component: alarmComponent,
+                    state: alarmComponent.getState()
+                };
+            });
+            alarmComponent.addEventListener('input', ev => this.onChildInput(ev));
         }
 
         onTimerFinished(ev) {
@@ -34,12 +41,22 @@
             const data = ev.detail.data;
             for (const item of data) {
                 const alarmComponent = document.createElement('alarm-component');
-                AlarmPage.alarms.push(alarmComponent);
+                //AlarmPage.alarms.push(alarmComponent);
                 this.shadowRoot.querySelector('main').appendChild(alarmComponent);
                 alarmComponent.onShadowRootReady(() => {
                     alarmComponent.setState(item.alarm);
+                    AlarmPage.alarms[alarmComponent.refId] = {
+                        component: alarmComponent,
+                        state: alarmComponent.getState()
+                    };
                 });
+                alarmComponent.addEventListener('input', ev => this.onChildInput(ev));
             }
+        }
+
+        onChildInput(ev) {
+            AlarmPage.alarms[ev.target.refId].state = ev.target.getState();
+            console.log(AlarmPage.alarms);
         }
     }
 
