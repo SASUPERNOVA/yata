75db09f81e8c9e9b382241d4d8920696ae9e5b79
SASUPERNOVA <agentrip@gmail.com>
2021-07-03 05:36:14 +0300

    Add extra fields to Reminder component, and remove initial Reminder component from ReminderPage

--- src/components/ReminderPage/ReminderPage.js
+++ src/components/ReminderPage/ReminderPage.js
@@ -3,6 +3,18 @@
         constructor() {
             super('components/ReminderPage/ReminderPage.html', 'reminder-page');
         }
+        
+        async connectedCallback() {
+            await super.connectedCallback();
+            this.shadowRoot.querySelector('add-button').addEventListener('addbutton-click', (ev) => this.onAddButtonClick(ev));
+        }
+
+        onAddButtonClick(ev) {
+            const reminderComponent = document.createElement('reminder-component');
+            this.shadowRoot.querySelector('main').appendChild(reminderComponent);
+            //reminderComponent.addEventListener('input', ev => this.onChildInput(ev));
+            //reminderComponent.onShadowRootReady(() => this.saveAlarms());
+        }
     }
 
     customElements.define('reminder-page', ReminderPage);
