4943d9173430403d963dbae1c33dac33b529f924
SASUPERNOVA <agentrip@gmail.com>
2021-07-04 10:54:37 +0300

    Add basic code structure for Reminder component, and event handling in ReminderPage component

--- src/components/Reminder/Reminder.js
+++ src/components/Reminder/Reminder.js
@@ -6,6 +6,55 @@
 
         async connectedCallback() {
             await super.connectedCallback();
+            this.setRefId(this.genURefId());
+            this.shadowRoot.querySelector('#delete-button').addEventListener('click', (ev) => this.deleteClick(ev));
+            this.props = {
+                datetimeInput: this.shadowRoot.querySelector('#datetime-input'),
+                toggleSwitch: this.shadowRoot.querySelector('toggle-switch'),
+                soundInput: this.shadowRoot.querySelector('#sound-input'),
+                repeatInput: this.shadowRoot.querySelector("#repeat-input"),
+                repeatTypeInput: this.shadowRoot.querySelector("#repeat-type-input"),
+                titleInput: this.shadowRoot.querySelector('#title-input'),
+                bodyInput: this.shadowRoot.querySelector('#body-input')
+            }
+            this.props.datetimeInput.addEventListener('change', (ev) => this.onDateTimeChange(ev));
+            this.props.toggleSwitch.addEventListener('change', ev => this.reminderSet(ev));
+            this.props.soundInput.addEventListener('input', ev => this.dispatchEvent(new Event('input')));
+            this.addEventListener('show-reminder', async (ev) => this.onShowReminder(ev));
+        }
+
+        deleteClick(ev) {
+            //const host = this.hostComponent();
+            this.remove();
+            //host.dispatchEvent(new CustomEvent('child-removed'));
+        }
+
+        onDateTimeChange(ev) {
+
+        }
+
+        reminderSet(ev) {
+
+        }
+
+        onShowReminder(ev) {
+
+        }
+
+        setState() {
+
+        }
+
+        getState() {
+
+        }
+
+        getDate() {
+            const time = this.props.datetimeInput.value;
+            const date = new Date(time);
+            date.setSeconds(0);
+            
+            return date;
         }
     }
 
--- src/components/ReminderPage/ReminderPage.js
+++ src/components/ReminderPage/ReminderPage.js
@@ -9,11 +9,30 @@
             this.shadowRoot.querySelector('add-button').addEventListener('addbutton-click', (ev) => this.onAddButtonClick(ev));
         }
 
-        onAddButtonClick(ev) {
+        async onAddButtonClick(ev) {
             const reminderComponent = document.createElement('reminder-component');
             this.shadowRoot.querySelector('main').appendChild(reminderComponent);
-            //reminderComponent.addEventListener('input', ev => this.onChildInput(ev));
+            reminderComponent.addEventListener('input', ev => this.onChildInput(ev));
             //reminderComponent.onShadowRootReady(() => this.saveAlarms());
+            const file = await fsAPI.loadFile('ReminderPage.json');
+            if (file) {
+                this.initialize(file.data);
+            }
+        }
+
+        initialize(data) {
+            for (const item of data) {
+                const reminderComponent = document.createElement('reminder-component');
+                this.shadowRoot.querySelector('main').appendChild(reminderComponent);
+                reminderComponent.onShadowRootReady(() => {
+                    reminderComponent.setState(item);
+                });
+                reminderComponent.addEventListener('input', ev => this.onChildInput(ev));
+            }
+        }
+
+        onChildInput(ev) {
+            console.log('Child Input');
         }
     }
 
