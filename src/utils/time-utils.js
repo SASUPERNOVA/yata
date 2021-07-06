e7c0fea519413e54991e6dfa3aef109446b0452c
SASUPERNOVA <agentrip@gmail.com>
2021-07-06 13:53:57 +0300

    Add time-utils.js

---
+++ src/utils/time-utils.js
@@ -0,0 +1,55 @@
+const TimeType = Object.freeze({
+    YEAR: 0,
+    MONTH: 1,
+    DAY: 2,
+    HOUR: 3,
+    MINUTE: 4,
+    SECOND: 5
+});
+
+function padNum(num) {
+    return num.toString().padStart(2, '0');
+}
+
+function toMilliseconds(time, timeType) {
+    switch (timeType) {
+        case TimeType.YEAR:
+            return time * 31557600000;
+        case TimeType.MONTH:
+            return time * 2629800000;
+        case TimeType.DAY:
+            return time * 86400000;
+        case TimeType.HOUR:
+            return time * 3600000;
+        case TimeType.MINUTE:
+            return time * 60000;
+        case TimeType.SECOND:
+            return time * 1000;
+        default:
+            return NaN;
+    }
+}
+
+function addTime(date, { years, months, days, hours, minutes, seconds, milliseconds }) {
+    let extraTime = 0;
+    extraTime += years ? toMilliseconds(years, TimeType.YEAR) : 0;
+    extraTime += months ? toMilliseconds(months, TimeType.MONTH) : 0;
+    extraTime += days ? toMilliseconds(days, TimeType.DAY) : 0;
+    extraTime += hours ? toMilliseconds(hours, TimeType.HOUR) : 0;
+    extraTime += minutes ? toMilliseconds(minutes, TimeType.MINUTE) : 0;
+    extraTime += seconds ? toMilliseconds(seconds, TimeType.SECOND) : 0;
+    extraTime += milliseconds ? milliseconds : 0;
+    return  new Date(date.getTime() + extraTime);
+}
+
+function subtractTime(date, { years, months, days, hours, minutes, seconds, milliseconds }) {
+    let extraTime = 0;
+    extraTime += years ? toMilliseconds(years, TimeType.YEAR) : 0;
+    extraTime += months ? toMilliseconds(months, TimeType.MONTH) : 0;
+    extraTime += days ? toMilliseconds(days, TimeType.DAY) : 0;
+    extraTime += hours ? toMilliseconds(hours, TimeType.HOUR) : 0;
+    extraTime += minutes ? toMilliseconds(minutes, TimeType.MINUTE) : 0;
+    extraTime += seconds ? toMilliseconds(seconds, TimeType.SECOND) : 0;
+    extraTime += milliseconds ? milliseconds : 0;
+    return  new Date(date.getTime() - extraTime);
+}
