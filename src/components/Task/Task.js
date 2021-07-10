bebd46ab20cf556c4ba9f6ed3e65ed05dce1e6f6
SASUPERNOVA <agentrip@gmail.com>
2021-07-10 18:54:52 +0300

    Add onRadioClick method in Task component

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -6,6 +6,29 @@
         
         async connectedCallback() {
             await super.connectedCallback();
+            this.props = {
+                timeInput: this.shadowRoot.querySelector('#time-input'),
+                toggleSwitch: this.shadowRoot.querySelector('toggle-switch'),
+                runCommandRadio: this.shadowRoot.querySelector('#run-command-radio'),
+                runFileRadio: this.shadowRoot.querySelector('#run-file-radio'),
+                runCommandInput: this.shadowRoot.querySelector('#run-command-input'),
+                runFile: this.shadowRoot.querySelector('#run-file'),
+                runFileInput: this.shadowRoot.querySelector('#run-file-input'),
+                argsInput: this.shadowRoot.querySelector('#args-input')
+            }
+            this.props.runCommandRadio.addEventListener('click', (ev) => this.onRadioClick(ev));
+            this.props.runFileRadio.addEventListener('click', (ev) => this.onRadioClick(ev));
+        }
+
+        onRadioClick(ev) {
+            if (ev.target == this.props.runCommandRadio) {
+                this.props.runCommandInput.classList.add('active');
+                this.props.runFile.classList.remove('active');
+            }
+            else {
+                this.props.runCommandInput.classList.remove('active');
+                this.props.runFile.classList.add('active');
+            }
         }
     }
 
