f58d540b7c59155a10b411dc56eab4e46d7a999c
SASUPERNOVA <agentrip@gmail.com>
2021-07-11 00:18:09 +0300

    Implement base methods of TaskPage component

--- src/components/TaskPage/TaskPage.js
+++ src/components/TaskPage/TaskPage.js
@@ -6,6 +6,40 @@
         
         async connectedCallback() {
             await super.connectedCallback();
+            this.shadowRoot.querySelector('add-button').addEventListener('addbutton-click', (ev) => this.onAddButtonClick(ev));
+            this.addEventListener('timer-finished', ev => this.onTimerFinished(ev));
+            this.addEventListener('child-removed', _ev => this.saveTasks());
+            const file = await fsAPI.loadFile('TaskPage.json');
+            if (file) {
+                this.initialize(file.data);
+            }
+        }
+
+        initialize(data) {
+            for (const item of data) {
+                const taskComponent = document.createElement('task-component');
+                this.shadowRoot.querySelector('main').appendChild(taskComponent);
+                taskComponent.onShadowRootReady(() => {
+                    taskComponent.setState(item);
+                });
+                taskComponent.addEventListener('input', ev => this.onChildInput(ev));
+            }
+        }
+
+        onAddButtonClick(ev) {
+            const taskComponent = document.createElement('task-component');
+            this.shadowRoot.querySelector('main').appendChild(taskComponent);
+            taskComponent.addEventListener('input', ev => this.onChildInput(ev));
+            taskComponent.onShadowRootReady(() => this.saveTasks());
+        }
+
+        onTimerFinished(ev) {
+            this.shadowRoot.querySelector(`task-component[ref-id="${ev.detail}"]`).dispatchEvent(new CustomEvent('run-task'));
+        }
+
+        saveTasks() {
+            const children = Array.from(this.shadowRoot.querySelector('main').children);
+            fsAPI.saveFile('TaskPage.json', {data: Array.from(children).map(task => task.getState())});
         }
     }
 
