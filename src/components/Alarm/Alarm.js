1eb7b6915f57fda48bc42c8586914b990a398ea5
SASUPERNOVA <agentrip@gmail.com>
2021-07-13 22:55:49 +0300

    Implement stopping Alarm component alarm on notification click

--- src/components/Alarm/Alarm.js
+++ src/components/Alarm/Alarm.js
@@ -24,10 +24,11 @@
 
         onRingAlarm(_ev) {
             const timeDiff = new Date().getTime() - this.getDate().getTime();
+            let notification;
             if (timeDiff < toMilliseconds(1, TimeType.MINUTE) && !document.modal) {
                 timerAPI.pauseClock();
                 if (this.props.titleInput.value || this.props.bodyInput.value) {
-                    new Notification(this.props.titleInput.value, { body: this.props.bodyInput.value });
+                    notification = new Notification(this.props.titleInput.value, { body: this.props.bodyInput.value });
                 }
                 if (this.props.soundInput.value) {
                     document.modal = document.createElement('alarm-modal');
@@ -42,6 +43,12 @@
                         delete document.modal;
                         timerAPI.resumeClock();
                     });
+                    notification.addEventListener('click', () => {
+                        body.removeChild(document.modal);
+                        audio.pause();
+                        delete document.modal;
+                        timerAPI.resumeClock();
+                    })
                 }
             }
         }
